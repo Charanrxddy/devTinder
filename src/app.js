85eef3b13aada7965e39a9afc2439e9ec4323ef6
charanrxddy <gopicharanreddygangula@gmail.com>
2025-02-03 17:55:06 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.use("/hello",(req,res)=>{
-    res.send("U asked for hello..?");
+app.get("/user",(req,res)=>{
+    res.send({firstname:"Gopicharanreddy", lastname: "Gangula"})
+});
+app.post("/user",(req,res)=>{
+    res.send("Saved to database");
 });
 
-app.use("/test",(req,res)=>{
-    res.send("U entered the server");
-});
-app.use("/",(req,res)=>{
-    res.send("Nothing to search");
-});
+// app.use("/hello",(req,res)=>{
+//     res.send("U asked for hello..?");
+// });
+
+// app.use("/test",(req,res)=>{
+//     res.send("U entered the server");
+// });
+// app.use("/",(req,res)=>{
+//     res.send("Nothing to search");
+// });
 
 app.listen(3000,()=>{
     console.log("listening on port 3000....");
