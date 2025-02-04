01456360d4f68cea1114469b5271d6f820416322
charanrxddy <gopicharanreddygangula@gmail.com>
2025-02-04 17:09:04 +0530

    Creating API's(User.find())

--- src/app.js
+++ src/app.js
@@ -4,13 +4,42 @@ const app = express();
 const {connectDb} = require("./config/database");
 const User = require("./models/user");
 
+app.use(express.json());
+
+app.get("/patch",async (req,res)=>{
+    try{
+        const allUsers = await User.find({});
+        console.log(allUsers);
+        if (allUsers){
+            res.send(allUsers);
+        }
+        else{
+            res.send("No Users Here")
+        }
+    }
+    catch(err){
+        res.status(404).send("something went wrong");
+    }
+});
+
+app.get("/users",async (req,res)=>{
+    try{
+        const email = req.body.emailId;
+        const findUserDetails = await User.find({emailId:email});
+        if (findUserDetails.length === 0){
+            res.send("No user found");
+        } 
+        else{
+            res.send(findUserDetails);
+        } 
+    }
+    catch(err){
+        res.status(404).send("something went wrong");
+    }
+})
+
 app.post("/users",async (req,res)=>{
-    const newUser = new User({
-        firstName:"Gopicharanreddy",
-        lastName :"Gangula",
-        emailId :"Gopicharanreddygangula@gmail.com",
-        password:"CFGHJ%^"
-    });
+    const newUser = new User(req.body);
     await newUser.save();
     res.send("posted successfully");
 });
